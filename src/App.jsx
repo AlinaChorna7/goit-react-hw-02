e6b4676d9724fc0e80f3207aee9bcb8cccf8b601
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-08-30 19:39:44 +0200

    end

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import Feedback from './components/Feedback/Feedback';
+import Options from './components/Options/Options';
+import Notification from './components/notification/Notification';
+
+
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedOptions = window.localStorage.getItem('feedback');
+    if (savedOptions !== null) {
+      return JSON.parse(savedOptions);
+    }
+    return { good: 0, neutral: 0, bad: 0 }; // Fixed initialization
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback === 0 ? (
+        <Notification /> 
+      ) : (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
+    </div>
+  );
+}
